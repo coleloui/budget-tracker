f9da57c619747b16b6580cd40e05f3d37516d04e
Louis Coleman <coleloui18@gmail.com>
2020-05-05 16:00:11 -0700

    working budget tracker

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # budget-tracker
+
+## Description
+This project is used to to have a working website that shows the your budget. You can adjust it on and offline.
+
+This project is deployed at https://sleepy-taiga-66644.herokuapp.com/
+
+---
+
+## Table of Contents
+*[Description](#description)
+
+*[Installation](#installation)
+
+*[GitHub](#github)
+
+---
+
+## Installation
+The user will need to init and npm i to install the dependencies of compression, express, lite-server, mongoose, and morgan.
+
+---
+
+## Usage for My Project
+The use of this project is to make it fast and easy for users to create and keep track of their money with this budget tool.
+
+![workout-tracker](./photo/workout.png)
+
+---
+
+## Contributors and Help
+Louis Coleman
+
+---
+
+## Tests
+There are no tests for this project
+
+---
+
+## GitHub
+
+![user github pic](https://avatars0.githubusercontent.com/u/16417094?v=4)
+
+
+https://github.com/coleloui
+
+
+No user email
+
+---
+                    
+                    
---
+++ public/db.js
@@ -0,0 +1,64 @@
+// const indexedDB =
+//   window.indexedDB ||
+//   window.mozIndexedDB ||
+//   window.webkitIndexedDB ||
+//   window.msIndexedDB ||
+//   window.shimIndexedDB;
+
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = (event) => {
+  const pending = request.result.createObjectStore("pending", {
+    autoIncrement: true,
+  });
+};
+
+request.onsuccess = (event) => {
+  db = event.target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log("Woops! " + event.target.error);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.objectStore("pending");
+
+  pendingStore.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.objectStore("pending");
+  const getAll = pendingStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
