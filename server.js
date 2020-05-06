07825d2d185732e81c4169770be253d3dd7db0fa
Louis Coleman <coleloui18@gmail.com>
2020-05-05 17:01:27 -0700

    server

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://<user1>:<password1>@ds217349.mlab.com:17349/heroku_0xwccknt", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://<user1>:<password1>@ds217349.mlab.com:17349/heroku_0xwccknt", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
